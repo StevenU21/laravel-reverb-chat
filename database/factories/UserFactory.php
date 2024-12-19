4dceecdaa932d09a5317cee6ab74efa6782aca53
StevenU21 <ulloadeifheltsteven@gmail.com>
2024-12-18 21:40:42 -0600

    update UserFactory to use a static password for user creation

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -28,7 +28,7 @@ class UserFactory extends Factory
             'name' => fake()->name(),
             'email' => $email,
             'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make($email),
+            'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
         ];
     }
